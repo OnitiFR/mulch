9153d61eb09ac1badc8c8e104bfafaf07102342a
Julien 'Xfennec' Lollivier <julien@clicproxy.com>
2018-07-25 15:28:15 +0200

    test-libvirt-storage: automatic creation of all storage pool and target dirs

---
+++ remove-storage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Crude script to remove all Mulch storage pool definitions, for test purposes
+# It's supposed to connect to qemu:///system [not session], so
+# run as root to make your life simplier)
+
+virsh pool-destroy mulch-releases
+virsh pool-destroy mulch-disks
+virsh pool-destroy mulch-cloud-init
+
+virsh pool-undefine mulch-releases
+virsh pool-undefine mulch-disks
+virsh pool-undefine mulch-cloud-init
--- test-libvirt-storage.go
+++ test-libvirt-storage.go
@@ -12,6 +12,62 @@ import (
 	"github.com/libvirt/libvirt-go-xml"
 )
 
+func GetOrCreateStoragePool(poolName string, poolPath string, mode string, conn *libvirt.Connect) (*libvirt.StoragePool, error) {
+	pool, err := conn.LookupStoragePoolByName(poolName)
+	if err != nil {
+		virtErr := err.(libvirt.Error)
+		if virtErr.Domain == libvirt.FROM_STORAGE && virtErr.Code == libvirt.ERR_NO_STORAGE_POOL {
+			fmt.Printf("pool '%s' not found, let's create it\n", poolName)
+			cwd, _ := os.Getwd()
+
+			xml, err := ioutil.ReadFile("test-storage.xml")
+			if err != nil {
+				return nil, err
+			}
+
+			poolcfg := &libvirtxml.StoragePool{}
+			err = poolcfg.Unmarshal(string(xml))
+			if err != nil {
+				return nil, fmt.Errorf("poolcfg.Unmarshal: %s", err)
+			}
+
+			poolcfg.Name = poolName
+			poolcfg.Target.Path = cwd + "/" + poolPath
+
+			if mode != "" {
+				poolcfg.Target.Permissions.Mode = mode
+			}
+
+			out, err := poolcfg.Marshal()
+			if err != nil {
+				return nil, fmt.Errorf("poolcfg.Marshal: %s", err)
+			}
+
+			pool, err = conn.StoragePoolDefineXML(string(out), 0)
+			if err != nil {
+				return nil, fmt.Errorf("StoragePoolDefineXML: %s", err)
+			}
+
+			pool.SetAutostart(true)
+			if err != nil {
+				return nil, fmt.Errorf("pool.SetAutostart: %s", err)
+			}
+
+			// WITH_BUILD = will create target directory if net exists
+			err = pool.Create(libvirt.STORAGE_POOL_CREATE_WITH_BUILD)
+			if err != nil {
+				return nil, fmt.Errorf("pool.Create: %s", err)
+			}
+		}
+	}
+
+	err = pool.Refresh(0)
+	if err != nil {
+		return nil, fmt.Errorf("pool.Refresh: %s", err)
+	}
+	return pool, nil
+}
+
 func main() {
 	conn, err := libvirt.NewConnect("qemu:///system")
 	if err != nil {
@@ -32,63 +88,35 @@ func main() {
 	// 	}
 	// 	pool.Free() // ?
 	// }
-	const poolName = "mulch-cloud-init"
-	const poolPath = "storage/cloud-init"
 
-	// const poolName = "mulch-releases"
-	// const poolPath = "storage/releases"
+	// I've seen strange things once in a while, like:
+	// - Code=38, Domain=0, Message='cannot open directory '…/storage/cloud-init': No such file or directory'
+	// - Code=55, Domain=18, Message='Requested operation is not valid: storage pool 'mulch-cloud-init' is not active
+	// Added more precise error messages to diagnose this.
 
-	// const poolName = "mulch-disks"
-	// const poolPath = "storage/disks"
-
-	pool, err := conn.LookupStoragePoolByName(poolName)
-	if err != nil {
-		virtErr := err.(libvirt.Error)
-		if virtErr.Domain == libvirt.FROM_STORAGE && virtErr.Code == libvirt.ERR_NO_STORAGE_POOL {
-			fmt.Println("no pool found, let's create it")
-			cwd, _ := os.Getwd()
-
-			xml, err := ioutil.ReadFile("test-storage.xml")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			poolcfg := &libvirtxml.StoragePool{}
-			err = poolcfg.Unmarshal(string(xml))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			poolcfg.Name = poolName
-			poolcfg.Target.Path = cwd + "/" + poolPath
-
-			out, err := poolcfg.Marshal()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			pool, err = conn.StoragePoolDefineXML(string(out), 0)
-			if err != nil {
-				log.Fatal(err)
-			}
-			pool.SetAutostart(true)
-			err = pool.Create(libvirt.STORAGE_POOL_CREATE_NORMAL)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-
-	err = pool.Refresh(0)
+	pool0, err := GetOrCreateStoragePool("mulch-cloud-init", "storage/cloud-init", "", conn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer pool0.Free()
 
-	vols, err := pool.ListAllStorageVolumes(0)
+	pool1, err := GetOrCreateStoragePool("mulch-releases", "storage/releases", "", conn)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, vol := range vols {
-		fmt.Println(vol.GetName())
+	defer pool1.Free()
+
+	pool2, err := GetOrCreateStoragePool("mulch-disks", "storage/disks", "0711", conn)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer pool2.Free()
+
+	// vols, err := pool.ListAllStorageVolumes(0)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for _, vol := range vols {
+	// 	fmt.Println(vol.GetName())
+	// }
 }
