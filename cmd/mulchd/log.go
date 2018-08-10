25948aa68b1bc9215f37e1df92b799b0e857b2a5
Julien 'Xfennec' Lollivier <julien@clicproxy.com>
2018-08-10 10:36:51 +0200

    add some missing doc

--- cmd/mulchd/log.go
+++ cmd/mulchd/log.go
@@ -6,11 +6,14 @@ import (
 	"github.com/Xfennec/mulch"
 )
 
+// Log provides error/warning/etc helpers for a Hub
 type Log struct {
 	target string
 	hub    *Hub
 }
 
+// NewLog creates a new log for the provided target and hub
+// note: mulch.MessageNoTarget is an acceptable target
 func NewLog(target string, hub *Hub) *Log {
 	return &Log{
 		target: target,
@@ -18,6 +21,7 @@ func NewLog(target string, hub *Hub) *Log {
 	}
 }
 
+// Log is a low-level function for sending a Message
 func (log *Log) Log(message *mulch.Message) {
 	// TODO: use our own *log.Logger (see log.go in Nosee project)
 	fmt.Printf("%s(%s): %s\n", message.Type, message.Target, message.Message)
@@ -25,22 +29,27 @@ func (log *Log) Log(message *mulch.Message) {
 	log.hub.Broadcast(message)
 }
 
+// Error sends a MessageError Message
 func (log *Log) Error(message string) {
 	log.Log(mulch.NewMessage(mulch.MessageError, log.target, message))
 }
 
+// Warning sends a MessageWarning Message
 func (log *Log) Warning(message string) {
 	log.Log(mulch.NewMessage(mulch.MessageWarning, log.target, message))
 }
 
+// Info sends an MessageInfo Message
 func (log *Log) Info(message string) {
 	log.Log(mulch.NewMessage(mulch.MessageInfo, log.target, message))
 }
 
+// Trace sends an MessageTrace Message
 func (log *Log) Trace(message string) {
 	log.Log(mulch.NewMessage(mulch.MessageTrace, log.target, message))
 }
 
+// SetTarget change the current "sending" target
 func (log *Log) SetTarget(target string) {
 	log.target = target
 }
