6a388439c13943838640db018e89dd0750d00862
Xfennec <xfennec@cqfd-corp.org>
2018-09-17 20:56:15 +0200

    fix WP restore

--- scripts/restore/wordpress.sh
+++ scripts/restore/wordpress.sh
@@ -9,4 +9,4 @@ rm "$HTML_DIR/index.php"
 tar xf "$_BACKUP/wordpress.tar" || exit $?
 
 # password on the command line? brrr…
-mysqldump -u $MYSQL_USER -h $MYSQL_HOST "-p$MYSQL_PASSWORD" $MYSQL_DB < "$_BACKUP/wordpress.sql" || exit $?
+mysql -u $MYSQL_USER -h $MYSQL_HOST "-p$MYSQL_PASSWORD" $MYSQL_DB < "$_BACKUP/wordpress.sql" || exit $?
