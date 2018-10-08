217e5d7fb96613c24d8325f9d9b11823ca156f58
Xfennec <xfennec@cqfd-corp.org>
2018-10-08 23:12:02 +0200

    fix HTTPS setting

--- scripts/prepare/deb-wordpress.sh
+++ scripts/prepare/deb-wordpress.sh
@@ -42,9 +42,11 @@ EOS
 # version 4.4.1 decided to switch to windows line endings
 sed -ri -e 's/\r$//' wp-config*
 
+
+# used as a test for next step ("awk HTTPS thingy")
 cp -f wp-config-sample.php wp-config.php || exit $?
 
-awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php || exit $? <<'EOPHP'
+awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
