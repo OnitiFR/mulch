281546d75aed6faeb8fb4fdc0cb91c1fe1bc2e7f
Xfennec <xfennec@cqfd-corp.org>
2018-10-15 23:21:55 +0200

    remove old experiments, update TODO

--- experiments/fakes/serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# go get github.com/vwochnik/gost
-
-tmux \
-    new-session 'cd test1 && gost -port 8081' \; \
-    split-window 'cd test2 && gost -port 8082' \; \
-    select-layout even-vertical
--- experiments/https-test/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-go build && scp https-test cobaye1:tmp && rm -f https-test
