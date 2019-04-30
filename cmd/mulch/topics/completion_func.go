33f3a30b1c634ad528b26890152c42177805db4f
Xfennec <xfennec@cqfd-corp.org>
2019-04-30 23:23:19 +0200

    add completion for 'do' command

--- cmd/mulch/topics/completion_func.go
+++ cmd/mulch/topics/completion_func.go
@@ -38,6 +38,24 @@ __internal_list_vms() {
     fi
 }
 
+__internal_list_actions() {
+    local mulch_output out vm_name
+    vm_name=$1
+    __mulch_get_server
+    if mulch_output=$(mulch --server $__mulch_current_server do $vm_name --basic 2>/dev/null); then
+        out=($(echo "${mulch_output}"))
+        COMPREPLY=( $( compgen -W "${out[*]}" -- "$cur" ) )
+    fi
+}
+
+__internal_doaction() {
+    if [ "$prev" =  "do" ]; then
+        __internal_list_vms
+    elif [ "${words[ $((c-2)) ]}" =  "do" ]; then
+        __internal_list_actions $prev
+    fi
+}
+
 __mulch_get_servers() {
     local out servers
     servers=$(egrep '^[[:blank:]]*name[[:blank:]]*=' ~/.mulch.toml | awk -F= '{print $2}')
@@ -51,16 +69,20 @@ __custom_func() {
             __internal_list_toml_files
             return
             ;;
-    mulch_ssh | mulch_vm_backup | mulch_vm_config | mulch_vm_delete | mulch_vm_infos | mulch_vm_lock | mulch_vm_rebuild | mulch_vm_redefine | mulch_vm_start | mulch_vm_stop | mulch_vm_unlock)
-    __internal_list_vms
-    return
-    ;;
+        mulch_ssh | mulch_vm_backup | mulch_vm_config | mulch_vm_delete | mulch_vm_infos | mulch_vm_lock | mulch_vm_rebuild | mulch_vm_redefine | mulch_vm_start | mulch_vm_stop | mulch_vm_unlock)
+            __internal_list_vms
+            return
+            ;;
         mulch_backup_cat | mulch_backup_delete | mulch_backup_download)
             __internal_list_backups
             return
             ;;
         mulch_backup_upload)
             __internal_list_qcow2_files
+            return
+            ;;
+        mulch_do)
+            __internal_doaction
             return
             ;;
         *)
