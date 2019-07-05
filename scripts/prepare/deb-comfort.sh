cbbbfadba8acb36401dcbf3619ab4e4d1f83cf02
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2019-07-05 18:06:40 +0200

    install powerline-gitstatus only when available

--- scripts/prepare/deb-comfort.sh
+++ scripts/prepare/deb-comfort.sh
@@ -6,7 +6,11 @@
 export DEBIAN_FRONTEND="noninteractive"
 sudo -E apt-get -y -qq install progress mc powerline locate man || exit $?
 
-# powerline-gitstatus (Ubuntu > 18.10)
+# powerline-gitstatus for Ubuntu > 18.10
+available=$(sudo apt-cache search --names-only '^powerline-gitstatus$' | wc -l)
+if [ $available -gt 0 ]; then
+    sudo -E apt-get -y -qq install powerline-gitstatus || exit $?
+fi
 
 # Set Midnight-Commander as the default editor, and apply
 # a cool setup to it
