1708cca653602fdf1520f98eb1caf454a860be49
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2019-10-16 10:27:46 +0200

    add some tips about URL updates in content

--- scripts/actions/wp_url.sh
+++ scripts/actions/wp_url.sh
@@ -7,8 +7,15 @@ set +a
 cd $HTML_DIR || exit $?
 
 url="https://$_DOMAIN_FIRST"
+old=$(wp option get home)
 
 wp option update home "$url" || exit $?
 wp option update siteurl "$url" || exit $?
 
 echo "* siteurl and home updated to $url"
+echo ""
+echo "You should now update URL in content."
+echo "minimal example:"
+echo "  wp search-replace '$old' '$url' --skip-columns=guid"
+echo ""
+echo "See also extensions like 'Velvet Blues Update URLs'."
