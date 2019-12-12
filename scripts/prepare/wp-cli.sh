aad38173f2bc8cfa2bd18452b58200a389ee2f44
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2019-12-12 18:02:45 +0100

    fix url-reset action on seeders

--- scripts/prepare/wp-cli.sh
+++ scripts/prepare/wp-cli.sh
@@ -4,13 +4,12 @@
 
 if [ -x bin/wp ]; then
   echo "wp-cli is already here"
-  exit 0
+else
+  echo "downloading and installing wp-cli"
+  mkdir -p bin || exit $?
+  curl -so bin/wp -fSL "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar" || exit $?
+  chmod +x bin/wp || exit $?
 fi
-
-echo "downloading and installing wp-cli"
-mkdir -p bin || exit $?
-curl -so bin/wp -fSL "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar" || exit $?
-chmod +x bin/wp || exit $?
 
 echo "_MULCH_ACTION_NAME=update"
 echo "_MULCH_ACTION_SCRIPT=https://raw.githubusercontent.com/OnitiFR/mulch/master/scripts/actions/wp_update.sh"
