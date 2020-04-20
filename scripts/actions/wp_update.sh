d924c27bd3ec5db60bbf068deba6aa33d598837d
Xfennec <xfennec@cqfd-corp.org>
2020-04-20 18:11:49 +0200

    do theme update as admin

--- scripts/actions/wp_update.sh
+++ scripts/actions/wp_update.sh
@@ -4,12 +4,17 @@ set -a
 . ~/env
 set +a
 
+# we will update themes as admin (need for some "premium" themes)
+tmpfile=$(mktemp /tmp/wp-update.XXXXXX)
+trap 'rm -f -- "$tmpfile"' INT TERM HUP EXIT
+echo '<?php define( "WP_ADMIN", true );' > $tmpfile
+
 cd $HTML_DIR || exit $?
 
 wp core update || exit $?
 wp plugin update --all || exit $?
 wp theme status || exit $? # refresh update cache
-wp theme update --all || exit $?
+wp theme update --all --require="$tmpfile" || exit $?
 wp language core update || exit $?
 wp language plugin update --all || exit $?
 wp language theme update --all || exit $?
