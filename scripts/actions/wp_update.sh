51d0a102b4a6a279a16f4db783fdb2bd95c45c18
Xfennec <xfennec@cqfd-corp.org>
2020-04-21 09:47:24 +0200

    remove WP_ADMIN=true theme update

--- scripts/actions/wp_update.sh
+++ scripts/actions/wp_update.sh
@@ -4,16 +4,15 @@ set -a
 . ~/env
 set +a
 
-# we will update themes as admin (need for some "premium" themes)
-tmpfile=$(mktemp /tmp/wp-update.XXXXXX)
-trap 'rm -f -- "$tmpfile"' INT TERM HUP EXIT
-echo '<?php define( "WP_ADMIN", true );' > $tmpfile
+# TODO: wp wc update? (if woocommerce detected)
+# TODO: find a way to update things like Divi and other "premium" themes / plugins
 
 cd $HTML_DIR || exit $?
 
 wp core update || exit $?
+wp core update-db || exit $?
 wp plugin update --all || exit $?
-wp theme update --all --require="$tmpfile" || exit $?
+wp theme update --all || exit $?
 wp language core update || exit $?
 wp language plugin update --all || exit $?
 wp language theme update --all || exit $?
