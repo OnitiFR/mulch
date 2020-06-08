2d44f98a990451bce970d6ba512168bef272ccea
Xfennec <xfennec@cqfd-corp.org>
2020-06-08 16:16:51 +0200

    add with-content argument to wp_url.sh

--- scripts/actions/wp_url.sh
+++ scripts/actions/wp_url.sh
@@ -18,9 +18,16 @@ wp option update home "$url" || exit $?
 wp option update siteurl "$url" || exit $?
 
 echo "* siteurl and home updated to $url"
-echo ""
-echo "You should now update URL in content."
-echo "minimal example:"
-echo "  wp search-replace '$old' '$url' --skip-columns=guid"
-echo ""
-echo "See also extensions like 'Velvet Blues Update URLs'."
+
+if [ $1 == 'with-content' ]; then
+    echo "Updating content:"
+    wp search-replace '$old' '$url' --skip-columns=guid || exit $?
+else
+    echo ""
+    echo "You should now update URL in content."
+    echo "minimal example:"
+    echo "  wp search-replace '$old' '$url' --skip-columns=guid"
+    echo "(next time, add 'with-content' argument, and I will do this automatically)"
+    echo ""
+    echo "See also extensions like 'Velvet Blues Update URLs'."
+fi
