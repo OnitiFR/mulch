ee3696d908f16605b95f990f72de7b3d0525d491
Xfennec <xfennec@cqfd-corp.org>
2020-06-08 16:37:41 +0200

    output 'you should update content' warning to stderr

--- scripts/actions/wp_url.sh
+++ scripts/actions/wp_url.sh
@@ -19,12 +19,12 @@ wp option update siteurl "$url" || exit $?
 
 echo "* siteurl and home updated to $url"
 
-if [ $1 == 'with-content' ]; then
+if [ "$1" == 'with-content' ]; then
     echo "Updating content:"
     wp search-replace '$old' '$url' --skip-columns=guid || exit $?
 else
     echo ""
-    echo "You should now update URL in content."
+    echo "You should now update URL in content." >&2
     echo "minimal example:"
     echo "  wp search-replace '$old' '$url' --skip-columns=guid"
     echo "(next time, add 'with-content' argument to do this)"
