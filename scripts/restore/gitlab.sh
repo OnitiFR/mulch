9b3cfc41d85e72a1fa1f64ba9ac45fc68cacd6b5
Julien Xfennec <xfennec@cqfd-corp.org>
2021-09-27 17:53:23 +0200

    add a missing error check

--- scripts/restore/gitlab.sh
+++ scripts/restore/gitlab.sh
@@ -12,7 +12,7 @@ sudo cp $_BACKUP/gitlab.rb /etc/gitlab/ || exit $?
 
 # update domain
 sudo sed -i "s|^external_url .*$|external_url 'http://$_DOMAIN_FIRST'|" /etc/gitlab/gitlab.rb || exit $?
-sudo sed -i "s|^gitlab_rails\['gitlab_ssh_host'\].*$|gitlab_rails\['gitlab_ssh_host'\] = '$_DOMAIN_FIRST'|" /etc/gitlab/gitlab.rb
+sudo sed -i "s|^gitlab_rails\['gitlab_ssh_host'\].*$|gitlab_rails\['gitlab_ssh_host'\] = '$_DOMAIN_FIRST'|" /etc/gitlab/gitlab.rb || exit $?
 
 sudo gitlab-ctl stop puma || exit $?
 sudo gitlab-ctl stop sidekiq || exit $?
