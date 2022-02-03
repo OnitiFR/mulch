597e332f041cb4fa8eb395e97f0163e4a6e62b1f
Julien Xfennec <xfennec@cqfd-corp.org>
2022-02-03 10:39:12 +0100

    generate aliases for inactive VMs with ssh-config -a

--- cmd/mulch/client/version.go
+++ cmd/mulch/client/version.go
@@ -1,7 +1,7 @@
 package client
 
 // Version of the client (x.y.z format)
-const Version = "1.6.5"
+const Version = "1.6.6"
 
 // ProtocolVersion implemented by this client
 const ProtocolVersion = 1
