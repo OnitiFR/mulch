187015e78fc56b6e2452c5f31cfbdccb6255e14f
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2022-02-18 18:25:40 +0100

    fix login shell detection

--- scripts/prepare/deb-comfort.sh
+++ scripts/prepare/deb-comfort.sh
@@ -54,7 +54,7 @@ EOS
 [ $? -eq 0 ] || exit $?
 
 sudo bash -c "cat > /etc/profile.d/is_locked.sh" <<- EOS
-if ! shopt -oq posix; then
+if [ "x\${BASH_VERSION-}" != x -a "x\${PS1-}" != x ]; then
   if [ "\$(is_locked)" = true ]; then
     echo -e "Warning: this VM is \e[41mlocked\e[0m!"
   fi
