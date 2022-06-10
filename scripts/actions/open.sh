5999b71dae431903961f3821cf597d49222689d6
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2022-06-10 17:54:09 +0200

    allow to specify a default page for 'open' action

--- scripts/actions/open.sh
+++ scripts/actions/open.sh
@@ -2,4 +2,6 @@
 
 # Run as app user
 
-echo "_MULCH_OPEN_URL=https://$_DOMAIN_FIRST"
+# You can define MULCH_OPEN_URL in the TOML file if needed
+
+echo "_MULCH_OPEN_URL=https://$_DOMAIN_FIRST/$MULCH_OPEN_URL"
