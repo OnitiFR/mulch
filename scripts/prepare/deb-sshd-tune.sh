4403c22f8aee07e784aa0f984b87fcddd4fe27ff
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2022-10-07 09:57:24 +0200

    add a sshd tuning script for availability

---
+++ scripts/prepare/deb-sshd-tune.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Tune OpenSSH server for a higher availability
+# in public-facing environments
+
+# -- Run as admin user
+
+function comment_setting() {
+    setting="$1"
+    file="$2"
+    sudo sed -e "/$setting/ s/^#*/#/" -i "$file" || exit ?
+}
+
+function add_line() {
+    text="$1"
+    file="$2"
+    echo "$text" | sudo tee -a "$file" > /dev/null || exit ?
+}
+
+file="/etc/ssh/sshd_config"
+
+comment_setting "LoginGraceTime" "$file"
+comment_setting "MaxStartups" "$file"
+comment_setting "MaxAuthTries" "$file"
+
+file="/etc/ssh/sshd_config.d/10-deb-sshd-tune.conf"
+sudo bash -c "cat > $file" <<- 'EOS'
+# added by deb-sshd-tune.sh
+LoginGraceTime 15
+MaxStartups 50:30:120
+MaxAuthTries 4
+EOS
+[ $? -eq 0 ] || exit $?
+
+sudo systemctl restart sshd || exit $?
