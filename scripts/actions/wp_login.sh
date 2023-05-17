05ca67d9ed959e6bc26208a55fa29f9113dd1a87
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2023-05-17 10:23:21 +0200

    login action now display a list of existing users when user is not found

--- scripts/actions/wp_login.sh
+++ scripts/actions/wp_login.sh
@@ -29,13 +29,25 @@ cat > $fullname <<- EOS
 require_once("../wp-config.php");
 require_once(ABSPATH . "/wp-load.php");
 
+unlink(__FILE__);
+
 \$user = get_userdatabylogin('${user_login}');
 if (!\$user) {
-    die('Unable to find the user ${user_login}.');
+    echo '<h3>Unable to find user "${user_login}"</h3>';
+    \$users = get_users([
+        'role__in' => ['administrator'],
+        'orderby' => 'display_name',
+        'order' => 'ASC',
+    ]);
+    echo '<ul>';
+    foreach (\$users as \$user) {
+        echo '<li>' . \$user->user_login . '</li>';
+    }
+    echo '</ul>';
+    die();
 }
 
 wp_set_auth_cookie(\$user->ID);
-unlink(__FILE__);
 header('Location: ' . parse_url(get_admin_url(), PHP_URL_PATH));
 EOS
 [ $? -eq 0 ] || exit $?
