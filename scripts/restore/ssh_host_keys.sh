fee5c9900c270454ed4fba3f7d3c568abb3a3187
Julien 'Xfennec' Lollivier <julien@oniti.fr>
2025-05-16 15:45:37 +0200

    fix sshd restart for Ubuntu 2404+

--- scripts/restore/ssh_host_keys.sh
+++ scripts/restore/ssh_host_keys.sh
@@ -6,5 +6,8 @@ cd /etc/ssh/ || exit $?
 
 sudo tar xf $_BACKUP/ssh-host-keys.tar || exit $?
 
-sudo systemctl restart sshd || exit $?
+ssh_unit=$(systemctl list-unit-files | cut -d\  -f1 | grep 'sshd*\.service' | head -n1)
+
+echo "Restarting $ssh_unit for new host keys"
+sudo systemctl restart $ssh_unit || exit $?
 
