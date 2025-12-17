a12819bc4df8cf72db365defda50cc217cecaf0d
Julien Xfennec <xfennec@cqfd-corp.org>
2025-12-17 17:24:16 +0100

    use HTTPS by default

--- scripts/restore/gitlab.sh
+++ scripts/restore/gitlab.sh
@@ -11,7 +11,7 @@ sudo cp $_BACKUP/gitlab-secrets.json /etc/gitlab/ || exit $?
 sudo cp $_BACKUP/gitlab.rb /etc/gitlab/ || exit $?
 
 # update domain
-sudo sed -i "s|^external_url .*$|external_url 'http://$_DOMAIN_FIRST'|" /etc/gitlab/gitlab.rb || exit $?
+sudo sed -i "s|^external_url .*$|external_url 'https://$_DOMAIN_FIRST'|" /etc/gitlab/gitlab.rb || exit $?
 sudo sed -i "s|^gitlab_rails\['gitlab_ssh_host'\].*$|gitlab_rails\['gitlab_ssh_host'\] = '$_DOMAIN_FIRST'|" /etc/gitlab/gitlab.rb || exit $?
 
 sudo gitlab-ctl stop puma || exit $?
