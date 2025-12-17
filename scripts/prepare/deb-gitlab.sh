f6503fe393a5e78cdf75aa533744c7a9bc32e76b
Julien Xfennec <xfennec@cqfd-corp.org>
2025-12-17 17:42:15 +0100

    disable internal let's encrypt cert generation

--- scripts/prepare/deb-gitlab.sh
+++ scripts/prepare/deb-gitlab.sh
@@ -7,8 +7,11 @@
 
 curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash || exit $?
 
+export EXTERNAL_URL="https://$_DOMAIN_FIRST"
+export GITLAB_OMNIBUS_CONFIG="letsencrypt['enable'] = false"
+
 if [ -n "$GITLAB_VERSION" ]; then
-    sudo EXTERNAL_URL="https://$_DOMAIN_FIRST" apt-get -y -qq install "gitlab-ce=$GITLAB_VERSION" || exit $?
+    sudo --preserve-env=EXTERNAL_URL,GITLAB_OMNIBUS_CONFIG apt-get -y -qq install "gitlab-ce=$GITLAB_VERSION" || exit $?
 else
-    sudo EXTERNAL_URL="https://$_DOMAIN_FIRST" apt-get -y -qq install gitlab-ce || exit $?
+    sudo --preserve-env=EXTERNAL_URL,GITLAB_OMNIBUS_CONFIG apt-get -y -qq install gitlab-ce || exit $?
 fi
